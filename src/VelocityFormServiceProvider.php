3512668ec2769db35e844408a6320b7088c50a6f
Rubrasum <josephbetbeze@gmail.com>
2024-10-16 19:40:53 -0400

    Fixed some problems I think, that were causing nothing to be loaded locally on project containing this one.

--- src/VelocityFormServiceProvider.php
+++ src/VelocityFormServiceProvider.php
@@ -2,33 +2,48 @@
 
 namespace Rubrasum\VelocityForms;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class VelocityFormServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register()
     {
+        // Merge package config into application config
         $this->mergeConfigFrom(
-            __DIR__.'/../config/velocityforms.php', 'velocityforms'
+            __DIR__ . '/../config/velocityforms.php',
+            'velocityforms'
         );
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'velocityforms');
-
+        // Register the package's publishable resources
         if ($this->app->runningInConsole()) {
             $this->publishes([
-//                __DIR__ . '/../config/velocityforms.php' => config_path('velocityforms.php'),
-//                __DIR__ . '/../resources/views' => resource_path('views/vendor/velocityforms'),
-//                __DIR__ . '/../dist' => public_path('vendor/velocity-forms'),
-//                __DIR__ . '/../database/migrations/' => database_path('migrations'),
-            ], 'velocityforms');
+                __DIR__ . '/../config/velocityforms.php' => App::configPath('velocityforms.php'),
+            ], 'velocityforms-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => App::resourcePath('views/vendor/velocityforms'),
+            ], 'velocityforms-views');
+
+            $this->publishes([
+                __DIR__ . '/../dist' => App::publicPath('vendor/velocity-forms'),
+            ], 'velocityforms-assets');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations/' => App::databasePath('migrations'),
+            ], 'velocityforms-migrations');
         }
-    }
-    // In your boot method:
+
+        // In your boot method:
 //        $this->publishes([
 //            __DIR__ . '/../config/velocityforms.php' => Config::get('app.config_path') . '/velocityforms.php',
 //        ], 'config');
@@ -40,5 +55,5 @@ class VelocityFormServiceProvider extends ServiceProvider
 //        $this->publishes([
 //            __DIR__ . '/../database/migrations' => database_path('migrations'),
 //        ], 'migrations');
-
+    }
 }
